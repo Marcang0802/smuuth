51bfa5807505eccf453445fc15315d97832c811b
marcus.ang.2024 <marcus.ang.2024@computing.smu.edu.sg>
2025-10-10 14:29:52 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    // data, functions
+    // template: "<h2>I am the template</h2>" /* btr to just put this in html */
+    data() {
+        return {
+            title: "The Final Empire"
+        }
+    }
+
+});
+
+app.mount("#app");  
