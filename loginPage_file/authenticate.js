36f1727588fedd3c7671008c14cb90af32f6c20c
Mch88-ai <mc.huang.2024@computing.smu.edu.sg>
2025-11-03 17:54:56 +0800

    added authenticate.js

---
+++ loginPage_file/authenticate.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByPrDz810_ttwZz0BdAJkeP54rMIqH3uw",
+  authDomain: "wad2-505be.firebaseapp.com",
+  projectId: "wad2-505be",
+  storageBucket: "wad2-505be.firebasestorage.app",
+  messagingSenderId: "221863917439",
+  appId: "1:221863917439:web:2a517ca11c83e991388c4a",
+  measurementId: "G-L69GG585GF"
+};
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export function checkLogin(){
+    onAuthStateChanged(auth,(user)=>{
+        if(!user){
+            alert('Acccess Denied')
+            window.location.replace('../landingPage.html')
+        }
+    })
+}
